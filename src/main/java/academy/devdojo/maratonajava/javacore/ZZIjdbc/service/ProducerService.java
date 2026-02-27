6e543d2af5cac4f487197f19f22d81201457a4ce
alibruno <alissonbruno318@gmail.com>
2026-02-26 23:19:00 -0300

    feat: ResultSet; ResultSetMetaData; DatabaseMetaData

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/service/ProducerService.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/service/ProducerService.java
@@ -30,6 +30,38 @@ public class ProducerService {
         ProducerRepository.update(p);
     }
 
+    public static List<Producer> findAll() {
+        return ProducerRepository.findAll();
+    }
+
+    public static List<Producer> findByName(String name) {
+        return ProducerRepository.findByName(name);
+    }
+
+    public static void showProducerMetaData() {
+        ProducerRepository.showProducerMetaData();
+    }
+
+    public static void showDriverMetaData() {
+        ProducerRepository.showDriverMetaData();
+    }
+
+    public static void showTypeScrollWorking(){
+        ProducerRepository.showTypeScrollWorking();
+    }
+
+    public static List<Producer> findByNameAndUpdateToUpperCase(String name) {
+        return ProducerRepository.findByNameAndUpdateToUpperCase(name);
+    }
+
+    public static List<Producer> findByNameAndInsertWhenNotFound(String name) {
+        return ProducerRepository.findByNameAndInsertWhenNotFound(name);
+    }
+
+    public static void findByNameAndDelete(String name) {
+        ProducerRepository.findByNameAndDelete(name);
+    }
+
     private static void requireValidId(Integer id) {
         if (id == null || id <= 0) {
             throw new IllegalArgumentException("Invalid value for id");
--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -2,16 +2,27 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerService;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class ConnectionFactoryTest01 {
+    private static final Logger log = LogManager.getLogger(ConnectionFactoryTest01.class);
+
     static void main() {
-        Producer producerToUpdate = Producer.builder()
-                .id(1)
-                .name("Bilbo")
-                .build();
+//        Producer producer = Producer.builder()
+//                .id(1)
+//                .name("Porfirio")
+//                .build();
 //        ProducerService.save(producer);
-        ProducerService.update(producerToUpdate);
+//        List<Producer> producers = ProducerService.findByName("il");
+//        log.info("Producers found '{}'", producers);
+//        ProducerService.showDriverMetaData();
+//        List<Producer> producers = ProducerService.findByName("il");
+        List<Producer> producers = ProducerService.findByNameAndUpdateToUpperCase("Mike");
+        log.info("Producers found '{}'", producers);
+
     }
 }
